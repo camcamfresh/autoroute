d6e81674e42236a7af93615875ef00a625c13b0c
Cameron <camcamfresh@gmail.com>
2021-07-05 00:10:22 -0500

    Add logs to init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,6 +3,10 @@
 # License: https://www.gnu.org/licenses/gpl-3.0.en.html
 # Purpose: Setup Wildcard Certificates for HTTPS Not Found Pages.
 
+# Copy Nginx default.conf
+echo "$0: Copying default.conf"
+cp /usr/share/nginx/defaults/default.conf /etc/nginx/conf.d/default.conf;
+
 # Check & Setup TLD Wildcard Certificates Options
 for DOMAIN in $DOMAINS; do
   if [ ! "$OPTIONS" ]; then
@@ -13,18 +17,18 @@ for DOMAIN in $DOMAINS; do
 done
 
 if [ -n "$OPTIONS" ]; then
-    /usr/share/nginx/scripts/requestSSL.sh --cert-name default $OPTIONS;
-else
-    echo "init.sh: No Domains Provided, skipping Not Found Page for HTTPS";
-    exit 1;
-fi
+  echo "$0: Requesting SSL Certificates for default.conf"
+  /usr/share/nginx/scripts/requestSSL.sh --cert-name default $OPTIONS;
 
-cp /usr/share/nginx/defaults/default.conf /etc/nginx/conf.d/default.conf;
-
-if [ ! -n $(cat /etc/nginx/conf.d/default.conf | sed -n '/listen 443 ssl/p') ]; then
+  if [ ! $(cat /etc/nginx/conf.d/default.conf | sed -n '/listen 443 ssl/p') ]; then
+    echo "$0: Add SSL to default.conf"
     sed -i /etc/nginx/conf.d/default.conf -E \
       -e 's/^([ \t]*)(listen 80;)$/\1\2\n\1listen 443 ssl;/' \
       -e 's/^([ \t]*)(location \/ \{)$/\1include \/etc\/nginx\/ssl.d\/default\/ssl.conf;\n\1\2/';
+  fi
+else
+  echo "$0: No Domains Provided, No Configuration for HTTPS";
+  exit 1;
 fi
 
 exit 0;
